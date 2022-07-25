b32325c2d6bebda84146c14f688b4ddd4fe4c239
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-25 17:30:20 -0400

    Finished todos

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,26 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+            [Theory]
+            [InlineData("radar", true)]
+            [InlineData("Kayak", true)]
+            [InlineData("quack", false)]
+            [InlineData("madam", true)]
+            [InlineData("noon", true)]
+            [InlineData("tooth", false)]
+        public void IsAPalindrome(string word, bool expected)
         {
+            // Arrange
+            var wordsmith = new Wordsmith();
 
+            // Act
+            var actual = wordsmith.IsAPalindrome(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
+        
+
+        
     }
 }
